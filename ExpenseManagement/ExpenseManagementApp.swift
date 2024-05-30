b6dfbc0e9d87aab7833e77dad1b9fb34fba86971
Felipe <felipe.m.f.peixoto@gmail.com>
2024-05-30 11:55:09 -0300

    Initial Commit

---
+++ ExpenseManagement/ExpenseManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagementApp.swift
+//  ExpenseManagement
+//
+//  Created by infra on 30/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
