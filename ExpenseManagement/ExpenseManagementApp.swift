1fd67be05a183d0c7eefea9a8a08462b98a9bbdf
Felipe <felipe.m.f.peixoto@gmail.com>
2024-07-15 16:39:53 -0300

    Icon and Splash Screen

--- ExpenseManagement/ExpenseManagementApp.swift
+++ ExpenseManagement/ExpenseManagementApp.swift
@@ -9,27 +9,41 @@ struct ExpenseManagementApp: App {
     @StateObject private var globalState = GlobalStateManager()
     @StateObject private var commonDataManager = CommonDataManager.instance
     
+    @State var didLoad = false
+    
     var body: some Scene {
         WindowGroup {
-            if authManager.isSignedIn {
-                TabViewContainer()
-                    .environmentObject(authManager)
-                    .environmentObject(globalState)
-                    .environmentObject(commonDataManager)
-                    .overlay(
-                        SwiftMessagesView()
-                            .environmentObject(globalState)
-                    )
-            } else {
-                ContentView()
-                    .environmentObject(authManager)
-                    .environmentObject(globalState)
-                    .environmentObject(commonDataManager)
-                    .overlay(
-                        SwiftMessagesView()
-                            .environmentObject(globalState)
-                    )
+            ZStack {
+                if authManager.isSignedIn {
+                    TabViewContainer()
+                        .environmentObject(authManager)
+                        .environmentObject(globalState)
+                        .environmentObject(commonDataManager)
+                        .overlay(
+                            SwiftMessagesView()
+                                .environmentObject(globalState)
+                        )
+                } else {
+                    ContentView()
+                        .environmentObject(authManager)
+                        .environmentObject(globalState)
+                        .environmentObject(commonDataManager)
+                        .overlay(
+                            SwiftMessagesView()
+                                .environmentObject(globalState)
+                        )
+                }
+                SplashScreenView()
+                    .opacity(didLoad ? 0 : 1)
+            }.onAppear {
+                // THis is where you put the condition to go to the app, out of the splash screen,  usually when somehting is done loading
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    withAnimation() {
+                        didLoad.toggle()
+                    }
+                }
             }
+            .animation(.default, value: didLoad)
         }
     }
 }
